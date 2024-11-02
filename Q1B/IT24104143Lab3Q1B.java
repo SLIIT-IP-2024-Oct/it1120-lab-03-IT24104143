7e48a5981fa08da70ca7b75adc3aaaffbf54bec7
IT24104143 <IT24104143@my.sliit.lk>
2024-11-02 22:54:55 +0530

    Add IT24104143Lab3Q1B.java to Q1B

--- Q1B/IT24104143Lab3Q1B.java
+++ Q1B/IT24104143Lab3Q1B.java
@@ -1,21 +1,18 @@
 import java.util.Scanner;
  
- public class IT24104143Lab3Q1B {
- public static void main(String [] args){
+ public class IT24104143Lab3Q1B{
+ public static void main (String[] args){
+
+   Scanner Scanner = new Scanner(System.in);
+   System.out.print("Enter the price of 1kg of rice :");
+   double priceperkg = Scanner.nextDouble();
+   System.out.print("Enter the number of kilograms you want to buy :");
+   double quantity = Scanner.nextDouble();
+   
+ // double Discount = 0.1;
   
-    Scanner Scanner = new Scanner(System.in);
-    System.out.print("Enter the monthly salary :");
-    double monthlySalary = Scanner.nextDouble();
-    System.out.print("Enter the number of OT hours  :");
-    double numberOfOThours = Scanner.nextDouble();
-    System.out.print("Enter the OT hourly rate  :");
-    double oThourlyrate = Scanner.nextDouble();
-	
-  // OT Amount = number Of OT hours*OT hourly rate
-   double OTAmount = numberOfOThours*oThourlyrate;
-  double TotalsalaryincludingOT = (monthlySalary+OTAmount);
-  System.out.println("The total Salary including OT is :" + TotalsalaryincludingOT);
-   }
+  double totalAmountWithDiscount = priceperkg*quantity - priceperkg*quantity*0.1;
+  System.out.println("    ");
+  System.out.println("The total amount with 10% discount is :   " + totalAmountWithDiscount);
+  }
  }
-  
-  
